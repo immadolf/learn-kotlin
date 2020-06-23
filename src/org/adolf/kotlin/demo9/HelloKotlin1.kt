1db61101f2fc7a1aacfae74f4ff06563e223997e
adolf <fqluowy@gmail.com>
2020-06-23 08:57:09 +0800

    - 注解的定义

---
+++ src/org/adolf/kotlin/demo9/HelloKotlin1.kt
@@ -0,0 +1,36 @@
+package org.adolf.kotlin.demo9
+
+/**
+ * 注解 annotation
+ *
+ * 通过元注解 meta-annotation 给注解添加信息
+ * @author 40313
+ * @date 2020/6/23
+ * @since
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+annotation class MyAnnotation
+
+@MyAnnotation
+class MyClass {
+
+    @MyAnnotation
+    fun myMethod(@MyAnnotation a: Int) = 10
+}
+
+class MyClass2(a: Int) {
+
+}
+
+// 加在构造方法上，此时不可省略constructor关键字
+class MyClass3 @MyAnnotation constructor(a: Int) {
+
+}
+
+class MyClass4 {
+
+    var a: Int? = null
+        @MyAnnotation set(value) {} //加在setter方法上
+}
